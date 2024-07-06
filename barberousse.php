5e460324ebd8cda47f30664ed93f7444a8e69fe4
barbaroussa <barbaroussa@ssh1.paris1.alwaysdata.com>
2024-07-06 20:46:51 +0200

    fixe name online

--- barberousse.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
-Plugin Name: Api torrent barberousse 
-Description: Api torrent barberousse
-Author: habib ouldmoussa
-*/
-require_once plugin_dir_path(__FILE__) . 'includes/function_barberousse.php';
