a4a4d05f3f4d87be45d6fb01bb26653807529963
Habib OULDMOUSSA <omhabibal@gmail.com>
2024-07-06 15:36:11 +0100

    init plugin

---
+++ barberousse.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: Api torrent barberousse 
+Description: Api torrent barberousse
+Author: habib ouldmoussa
+*/
+require_once plugin_dir_path(__FILE__) . 'includes/function_barberousse.php';
